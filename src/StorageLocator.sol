b4769c9079535341beb2120cd0864004030db80b
kadenzipfel <kaden.zipfel@hotmail.com>
2022-02-20 13:51:46 -0800

    StorageLocator.sol

--- src/StorageLocator.sol
+++ src/StorageLocator.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-contract StorageLocator {
+library StorageLocator {
+  function arrayLocator(uint slot, uint index, uint size)
+    public
+    pure
+    returns (uint)
+  {
+      return uint(keccak256(abi.encodePacked(slot))) + (index * size);
+  }
+
+  function mappingLocator(uint key, uint slot) public pure returns (uint) {
+    return uint(keccak256(abi.encodePacked(key, slot)));
+  }
 }
